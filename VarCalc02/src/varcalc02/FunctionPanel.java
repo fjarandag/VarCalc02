4c7b3ec8b165b68fbc791356f0aa3633ad2837fc
Javier Aranda <fjarandag@gmail.com>
2015-02-23 11:14:29 +0100

    correct+document

--- VarCalc02/src/varcalc02/FunctionPanel.java
+++ VarCalc02/src/varcalc02/FunctionPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.text.NumberFormat;
 import java.text.ParseException;
 
 import javax.swing.BorderFactory;
@@ -42,11 +43,18 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 		// No further initialization until formula provided
 	}
 	
+	/**
+	 * Set the function object into the panel.
+	 * The panel components are re-deployed.
+	 * @param newFunction
+	 */
 	public void setFunction(Function newFunction) {
 		this.function = newFunction;
 		
+		// Just in case there was a previous deployment
 		removeAll();
 		
+		// Display name and description
 		setBorder(BorderFactory.createTitledBorder(function.getCaption()));
 
 		if (function.getDescription() != null && !function.getDescription().trim().isEmpty()) {
@@ -54,6 +62,8 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 					GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, // anchor, fill
 					new Insets(2, 2, 2, 2), 2, 2)); // insets, ipadx, ipady
 		}
+		
+		// Deploy the variables' components
 		
 		FunctionVariable[] variables = newFunction.getVariables();
 		rbVariables = new JRadioButton[variables.length];
@@ -63,20 +73,27 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 		// Hack casting to allow create array of generic type http://stackoverflow.com/questions/14917375/cannot-create-generic-array-of-how-to-create-an-array-of-mapstring-obje
 		combosUnits = (JComboBox<String>[]) new JComboBox[variables.length];
 		valuesVariables = new double[variables.length];
+		// current units applied to variables, if no type specified a neutral unit is used.
 		unitsSelected = new VariableTypeUnit[variables.length];
 //		unitsCalculate = new VariableTypeUnit[variables.length];
 		
 		selectedVariable = 0;
 		
-		final int yOffset = 1;
+		final int yOffset = 1; // gridbag layout y coordinate for first variable
 		for (int i = 0; i < variables.length; i++) { // foreach variable. Need the index
 			FunctionVariable atVariable = variables[i];
 			valuesVariables[i] = atVariable.getInitialDisplayValue();
-			VariableType varType = function.getVariableType(atVariable.getType());
-			// TT-LOW handle null properties
-			unitsSelected[i] = varType.getUnit(atVariable.getInitialDisplayUnit());
-//			unitsCalculate[i] = varType.getUnit(atVariable.getFunctionUnit());
+			VariableType varType = null; // optional, null if no type specified -> No combo
+			if (atVariable.getType() != null) {
+				varType = function.getVariableType(atVariable.getType());
+				unitsSelected[i] = varType.getUnit(atVariable.getInitialDisplayUnit());
+//				unitsCalculate[i] = varType.getUnit(atVariable.getFunctionUnit());
+			} else { // set unit(parse/format; value conversion)
+				unitsSelected[i] = new VariableTypeUnit.Proportional("default","", // name, caption
+						NumberFormat.getNumberInstance(), 1.0, 0.0); // NumberFormat, factor, offset
+			}
 			
+			// Radio button (includes label) for #1 Displaying the variable name and #2 Allowing to select the calculated variable
 			// TT-FUTURE disable for oneway functions
 			JRadioButton rb = new JRadioButton(atVariable.getCaption());
 			rbVariables[i] = rb;
@@ -89,6 +106,7 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 					GridBagConstraints.WEST, GridBagConstraints.NONE, // anchor, fill
 					new Insets(2, 2, 2, 2), 2, 2)); // insets, ipadx, ipady
 			
+			// TextField for displaying/entering variable value
 			JTextField field = new JTextField();
 			fieldsVariables[i] = field;
 			updateFieldTextFromCalcValue(i);
@@ -102,29 +120,36 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 					GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, // anchor, fill
 					new Insets(2, 2, 2, 2), 2, 2)); // insets, ipadx, ipady
 			
-			// TT-LOW behavior for no type, no units, single unit, disabled units ... [52]
-			// Fill in combo choices, and find out the initially selected index.
-			String[] unitChoices = new String[varType.getUnits().length];
-			int iUnitSelected = -1;
-			String unitSelectedName = atVariable.getInitialDisplayUnit();
-			for (int iunit = 0; iunit < unitChoices.length; iunit++) {
-				unitChoices[iunit] = varType.getUnits()[iunit].getCaption();
-				if (varType.getUnits()[iunit].getName().equals(unitSelectedName)) { iUnitSelected = iunit; }
+			// available units (e.g. meters-feet) if a type is specified for the variable
+			if (varType != null) {
+				// TT-LOW behavior for no type, no units, single unit, disabled units ... [52]
+				// Fill in combo choices, and find out the initially selected index.
+				String[] unitChoices = new String[varType.getUnits().length];
+				int iUnitSelected = -1;
+				String unitSelectedName = atVariable.getInitialDisplayUnit();
+				for (int iunit = 0; iunit < unitChoices.length; iunit++) {
+					unitChoices[iunit] = varType.getUnits()[iunit].getCaption();
+					if (varType.getUnits()[iunit].getName().equals(unitSelectedName)) { iUnitSelected = iunit; }
+				}
+				assert iUnitSelected > -1;
+				JComboBox<String> comboUnits = new JComboBox<String>(unitChoices);
+				combosUnits[i] = comboUnits;
+				comboUnits.setSelectedIndex(iUnitSelected);
+				comboUnits.setEditable(false);
+				comboUnits.addActionListener(this);
+				add(comboUnits, new GridBagConstraints(2, i + yOffset, 1, 1, 0.0, 0.0, // gridx, gridy, gridwidth, gridheight, weightx, weighty
+						GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, // anchor, fill
+						new Insets(2, 2, 2, 2), 2, 2)); // insets, ipadx, ipady
 			}
-			assert iUnitSelected > -1;
-			JComboBox<String> comboUnits = new JComboBox<String>(unitChoices);
-			combosUnits[i] = comboUnits;
-			comboUnits.setSelectedIndex(iUnitSelected);
-			comboUnits.setEditable(false);
-			comboUnits.addActionListener(this);
-			add(comboUnits, new GridBagConstraints(2, i + yOffset, 1, 1, 0.0, 0.0, // gridx, gridy, gridwidth, gridheight, weightx, weighty
-					GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, // anchor, fill
-					new Insets(2, 2, 2, 2), 2, 2)); // insets, ipadx, ipady
-			
 		} // foreach variable
 		// TT-LOW variable values textfields could have consecutive tab indices, so they would be navigated more conveniently with keyboard
 	}
 	
+	/**
+	 * When a text field value is changed, the value is parsed and copied into the array of values.
+	 * @param index Index of the variable/field with new value entered.
+	 * @throws ParseException
+	 */
 	protected void updateCalcValueFromFieldText(int index) throws ParseException {
 		String fieldText = fieldsVariables[index].getText();
 		double fieldValue = ((Number)unitsSelected[index].parse(fieldText)).doubleValue();
@@ -133,7 +158,11 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 //		valuesVariables[index] = calcValue;
 		valuesVariables[index] = coreValue;
 	}
-	
+
+	/**
+	 * Update the value displayed in a field (when calculated/loaded in the variables array).
+	 * @param index
+	 */
 	protected void updateFieldTextFromCalcValue(int index) {
 //		double coreValue = unitsCalculate[index].valueToCore(valuesVariables[index]);
 //		double fieldValue = unitsSelected[index].valueFromCore(coreValue);
@@ -142,7 +171,10 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 		fieldsVariables[index].setText(fieldText);
 	}
 	
-	
+	/**
+	 * Set which variable should be calculated when the other variables are entered.
+	 * @param newVariableIndex
+	 */
 	public void setSelectedVariable(int newVariableIndex) {
 		if (selectedVariable != newVariableIndex) {
 			fieldsVariables[selectedVariable].setEditable(true);
@@ -158,6 +190,10 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 		}
 	}
 	
+	/**
+	 * Calculate the target variable and display its textfield.
+	 * Executed once the variables array is updated.
+	 */
 	public void calculateVariable() {
 		try {
 			// TT-LOW fetch engine
@@ -169,7 +205,12 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 		}
 	}
 	
+	/**
+	 * Invoked when a text field is updated
+	 * @param fieldIndex
+	 */
 	private void onFieldUpdated(int fieldIndex) {
+		// TT-LOW Might check if value is actually changed (although re-calculating is not so expensive)
 		try {
 			updateCalcValueFromFieldText(fieldIndex);
 		} catch (ParseException e) {
@@ -190,7 +231,7 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 	
 	@Override
 	public void focusGained(FocusEvent e) {
-		// when field gains focus, select text to edit faster
+		// when field gains focus, select text to edit/copy faster
 		Object oSrc = e.getSource();
 		if (oSrc instanceof JTextField) {
 			((JTextField)oSrc).selectAll();
@@ -214,6 +255,7 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object oSrc = e.getSource();
+		// When radio button is activated, that variable is selected as target variable
 		if (oSrc instanceof JRadioButton) {
 			for (int i = rbVariables.length - 1; i >= 0; i--) {
 				if (rbVariables[i] == oSrc) {					
@@ -221,6 +263,7 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 					break;
 				}
 			}
+		// When a new value is entered in a field
 		} else if (oSrc instanceof JTextField) {
 			for (int i = fieldsVariables.length - 1; i >= 0; i--) {
 				if (fieldsVariables[i] == oSrc) {
@@ -228,6 +271,7 @@ public class FunctionPanel extends JPanel implements ActionListener, FocusListen
 					break;
 				}
 			}
+		// When a unit is selected for a variable
 		} else if (oSrc instanceof JComboBox) {
 			for (int i = combosUnits.length - 1; i >= 0; i--) {
 				if (combosUnits[i] == oSrc) {
