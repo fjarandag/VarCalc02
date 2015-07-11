5442202dc129c60af93db2fa7de2622738339d79
Javier Aranda <fjarandag@gmail.com>
2015-07-11 12:34:27 +0200

    GPL2 license applied to HTML

--- VarCalc-HTML5/WebContent/dynamic01/calculator.js
+++ VarCalc-HTML5/WebContent/dynamic01/calculator.js
@@ -1,5 +1,21 @@
-// JavaScript for multi-way formula calculators
-// CC BY SA Javier Aranda 2015
+/*
+Mortgage Calculator. Prototype of dynamic multi-directional calculator by Javier Aranda.
+Dynamic: Formula/Equation specified at runtime. Multi-directional: Many of the variables are selectable as target.
+Copyright (c) 2015 Javier Aranda (http://javier-aranda.com/).
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 // ?? Depend on jQuery 1.7 or compatible loaded
 
--- VarCalc-HTML5/WebContent/irr/irr.js
+++ VarCalc-HTML5/WebContent/irr/irr.js
@@ -1,6 +1,20 @@
-// JavaScript for Internal Rate of Return Calculator
-// CC BY SA Javier Aranda 2015 http://creativecommons.org/licenses/by-sa/3.0/
+/*
+IRR Calculator: Calculates Rate of Return, or any other variable, for an arbitrary number of flows.
+Copyright (c) 2015 Javier Aranda (http://javier-aranda.com/).
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 // Depends on jQuery 1.7 or compatible
 /* ToC
 - addSinglePayment()
